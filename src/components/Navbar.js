7999cd0fcca73b7d190f2272f9b7899eecd48371
Doddanna <doddannab17@gmail.com>
2024-11-10 23:49:23 +0530

    Navbar updated with dynamic data support

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,7 @@
 import React from "react";
+import logo from "../images/logo.svg";
+import { navBarLinks } from "../data/data";
+import { socialMediaLinks } from "../data/data";
 
 const Navbar = () => {
   return (
@@ -6,70 +9,40 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="nav-center">
           <div className="nav-header">
-            <img src="./images/logo.svg" className="nav-logo" alt="backroads" />
+            <img src={logo} className="nav-logo" alt="backroads" />
             <button type="button" className="nav-toggle" id="nav-toggle">
               <i className="fas fa-bars"></i>
             </button>
           </div>
           {/* <!-- left this comment on purpose --> */}
           <ul className="nav-links" id="nav-links">
-            <li>
-              <a href="#home" className="nav-link">
-                {" "}
-                home{" "}
-              </a>
-            </li>
-
-            <li>
-              <a href="#about" className="nav-link">
-                {" "}
-                about{" "}
-              </a>
-            </li>
-
-            <li>
-              <a href="#services" className="nav-link">
-                {" "}
-                services{" "}
-              </a>
-            </li>
-
-            <li>
-              <a href="#tours" className="nav-link">
-                {" "}
-                tours
-              </a>
-            </li>
+            {navBarLinks.map((link) => {
+              return (
+                <li>
+                  <a href={link.href} className="nav-link" key={link.id}>
+                    {" "}
+                    {link.name}{" "}
+                  </a>
+                </li>
+              );
+            })}
           </ul>
 
           <ul className="nav-icons">
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-squarespace"></i>
-              </a>
-            </li>
+            {socialMediaLinks.map((link) => {
+              return (
+                <li>
+                  <a
+                    href={link.href}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="nav-icon"
+                  >
+                    <i className={link.icon}></i>
+                  </a>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </nav>
---
+++ src/data/data.js
@@ -0,0 +1,40 @@
+export const navBarLinks = [
+  {
+    id: 1,
+    href: "#home",
+    name: "home",
+  },
+  {
+    id: 2,
+    href: "#about",
+    name: "about",
+  },
+  {
+    id: 3,
+    href: "#services",
+    name: "services",
+  },
+  {
+    id: 4,
+    href: "#tours",
+    name: "Tours",
+  },
+];
+
+export const socialMediaLinks = [
+  {
+    id: 1,
+    href: "https://www.facebook.com",
+    icon: "fab fa-facebook",
+  },
+  {
+    id: 2,
+    href: "https://www.twitter.com",
+    icon: "fab fa-twitter",
+  },
+  {
+    id: 3,
+    href: "https://www.squarespace.com",
+    icon: "fab fa-squarespace",
+  },
+];
